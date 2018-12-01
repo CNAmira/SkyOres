1333619f68a0a9e27e13f034788996e8480513c2
Li Keqing <auroracloud4096@gmail.com>
2018-12-01 13:20:54 +0800

    Use forEach instead of for(:)

--- src/main/java/io/github/cnamira/SkyOres/Listeners/GenerateOres.java
+++ src/main/java/io/github/cnamira/SkyOres/Listeners/GenerateOres.java
@@ -10,6 +10,7 @@ import org.bukkit.event.block.BlockFormEvent;
 
 import java.security.SecureRandom;
 import java.util.*;
+import java.util.function.BiConsumer;
 
 public class GenerateOres implements Listener {
     private SkyOres addon;
@@ -21,24 +22,21 @@ public class GenerateOres implements Listener {
     public GenerateOres(SkyOres addon) {
         this.addon = addon;
 
-        ConfigurationSection section = addon.getConfig().getConfigurationSection("ores");
-        Set<String> keys = section.getKeys(false);
-        for (String key : keys) {
+        addon.getConfig().getConfigurationSection("ores").getValues(false).forEach((key, _weight) -> {
             Material material = Material.matchMaterial(key);
-            int weight = section.getInt(key);
+            int weight = (Integer) _weight;
             Ore ore = new Ore(material, weight);
-            sum += weight;
             ores.add(ore);
-        }
+            sum += weight;
+        });
 
-        List<String> worlds = addon.getConfig().getStringList("worlds");
-        for (String world : worlds) {
+        addon.getConfig().getStringList("worlds").forEach(world -> {
             if (addon.getServer().getWorld(world) != null) {
-                this.worlds.add(world);
+                worlds.add(world);
             } else {
                 this.addon.getLogger().warning("World name " + world + " does NOT exist, ignoring.");
             }
-        }
+        });
     }
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
@@ -46,7 +44,6 @@ public class GenerateOres implements Listener {
         if (this.worlds.contains(event.getNewState().getWorld().getName())) {
             if (event.getNewState().getType().equals(Material.COBBLESTONE) || event.getNewState().getType().equals(Material.STONE)) {
                 event.getNewState().setType(getRandomMaterial());
-                return;
             }
         }
     }
